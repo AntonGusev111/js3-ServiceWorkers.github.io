5b3f2dba58cb12cfbc7f4359941e3be274d8a6e2
GusevADresume <resumeGusevAD@gmail.com>
2023-01-30 14:42:13 +0300

    nw

--- src/js/serviceWorker.js
+++ src/js/serviceWorker.js
@@ -1,7 +1,17 @@
 self.addEventListener('install', (e) =>{
     console.log("Установлен");
 
-});
+    e.waitUntil(
+        caches.open('my-best-cache')
+          .then((cache) => {
+            cache.addAll([
+              './',
+              './index.html',
+              './css/style.css',
+            ])
+          })
+      )
+    });
 
 self.addEventListener('activate', (e) =>{
     console.log("Активирован")
@@ -9,4 +19,7 @@ self.addEventListener('activate', (e) =>{
 
 self.addEventListener('fetch', (e) =>{
     console.log("Происходит запрос на сервер")
+    e.respondWith(
+        caches.match(e.request)
+    )
 })
